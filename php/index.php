90fb6270363a80dbf81c263a9e75dba588c739b4
lanorbug <joseph.awr@gmail.com>
2021-09-26 15:10:52 +0200

    Ajout du premier fichier

---
+++ php/index.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Accueil - EWakfu</title>
+    <link  rel="stylesheet" href="../scss/css/stylecompil.css"/>
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css"
+          integrity="sha384-DyZ88mC6Up2uqS4h/KRgHuoeGwBcD4Ng9SiP4dIRy0EXTlnuz47vAwmeGwVChigm"
+          crossorigin="anonymous">
+</head>
+<body>
+
+<div id="body-container">
+
+    <div id="container-mininav">
+
+        <div id="mininav">
+
+            <div class="icone-home">
+                <i class="fas fa-door-open"></i>
+            </div>
+            <div class="icone-profil">
+                <i class="fas fa-user-alt"></i>
+            </div>
+            <div class="icone-connect">
+                <i class="fas fa-power-off"></i>
+            </div>
+
+
+        </div>
+
+    </div>
+
+
+
+    <div id="container-principal">
+
+        <div id="nav-principal">
+
+            <div class="bloc-builder">
+                <p>Builder</p>
+            </div>
+            <div class="bloc-encyclo">
+                <p>Encyclopedie</p>
+            </div>
+            <div class="bloc-almanax">
+                <p>Almanax</p>
+            </div>
+            <div class="bloc-profil">
+                <p>Profil</p>
+            </div>
+
+        </div>
+
+    </div>
+
+</div>
+
+
+</body>
+</html>
